bebd3e62cc54905af8836788595f9d434dd226ce
Connor Wheeler <Connorthevaliant@gmail.com>
2025-02-27 22:54:42 -0700

    Attacking Delay and State Machine prep
    Created the logic for attack delays and did the preliminary stuff for a direction state machine

--- Rat-Game/Assets/Scripts/Playermovemnet.cs
+++ Rat-Game/Assets/Scripts/Playermovemnet.cs
@@ -1,25 +1,34 @@
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 7f;
-    private Rigidbody rb;
-    private VisualEffect atc;
-    private Vector3 movement;
-    private bool isGrounded;
+    public float moveSpeed = 5f;            //Movement speed
+    public float jumpForce = 7f;            //Jump strength
+    private Rigidbody rb;                   //Rigidbody of the player gameobject
+    private VisualEffect atc;               //VisualEffect component that creates the player's attacks
+    private Vector3 movement;               //movement vector based on player inputs
+    private bool isGrounded;                //state flag that the player is on the ground
+    private bool idle;                      //state flag that the player is not moving
+    private bool jumpInput;                 //state flag that the player has pressed the jump button
     
-    private bool jumpInput;
-    private bool attackInput;
-    private bool canAttack = true;
+    private bool attackInput;               //state flag that the player has pressed the attack button
+    private bool canAttack = true;          //state flag of weather or not the player can attack
+    public float attackDelay = 1f;          //delay between attacks in seconds
+    private float attackCounter;            //counted progress of the delay between attacks in seconds
+    private int facing;                     //state holder for the direction the player is facing
 
-    private enum Direction 
+    private enum Direction                  //Enum for the direction the player is facing
     {
-        North,
-        East,
-        South,
-        West
+        North, //Facing the +X direction (Right on the Camera)
+        NorthEast,
+        East, //Facing the +Z direction (Away From the Camera)
+        SouthEast,
+        South, //Facing the -X direction (Left on the Camera)
+        SouthWest,
+        West, //Facing the -Z direction (Towards the Camera)
+        NorthWest
     }
 
     void Start()
@@ -39,11 +48,21 @@ public class PlayerMovement : MonoBehaviour
         if (attackInput && canAttack) {
             Attack();
         }
+        else if (!canAttack) {
+            attackCounter += Time.fixedDeltaTime;
+
+            if (attackCounter >= attackDelay) {
+                canAttack = true;
+                attackCounter = 0;
+            }
+        } 
     }
 
     void FixedUpdate()
     {
+        DirectionCheck();
         MovePlayer();
+        
     }
 
     void CheckInputs() {
@@ -51,7 +70,6 @@ public class PlayerMovement : MonoBehaviour
         movement.z = Input.GetAxis("Vertical");
         jumpInput = Input.GetButtonDown("Jump");
         attackInput = Input.GetButtonDown("Fire1");
-
 
     }
 
@@ -68,7 +86,48 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Attack() {
+        canAttack = false;
         atc.Play();
+    }
+
+    void DirectionCheck() { //Direction State Machine
+        if (movement.x == 0 && movement.z == 0) {
+            idle = true;
+        }
+        else if (movement.x > 0) { //North
+            if (movement.z > 0) {
+                facing = (int) Direction.NorthEast;
+            }
+            else if (movement.z < 0) {
+                facing = (int) Direction.NorthWest;
+            }
+            else
+            {
+                facing = (int) Direction.North;
+            }
+        }
+        else if (movement.x < 0) { //South
+            if (movement.z > 0) {
+                facing = (int) Direction.SouthEast;
+            }
+            else if (movement.z < 0) {
+                facing = (int) Direction.SouthWest;
+            }
+            else {
+                facing = (int) Direction.South;
+            }
+        }
+        else { //East-West
+            if (movement.z > 0) {
+                facing = (int) Direction.East;
+            }
+            else if (movement.z < 0) {
+                facing = (int) Direction.West;
+            }
+            else {
+                Debug.Log("Fix Your Damn Code: Direction State Machine Reached Illegal Location");
+            }
+        }
     }
 
     void OnCollisionEnter(Collision collision)
