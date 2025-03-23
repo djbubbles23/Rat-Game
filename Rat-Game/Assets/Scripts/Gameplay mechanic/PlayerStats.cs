35a77424bbc5b36e141aa1339c910c128aa4164d
VTNguyen02 <vnguye55@asu.edu>
2025-03-23 12:16:34 -0700

    Death Menu and Pause menu changes
    add "Main Menu" button to pause and death menus
    
    added "back" button to map menu
    
    still need to fix pause menu when playing, its 50/50 rn

--- Rat-Game/Assets/Scripts/Gameplay mechanic/PlayerStats.cs
+++ Rat-Game/Assets/Scripts/Gameplay mechanic/PlayerStats.cs
@@ -50,7 +50,7 @@ public class PlayerStats : MonoBehaviour
         {
             Debug.Log("PLAYER DIED!");
             clip = deathSound;
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(5);
         }
         
         audioSource.PlayOneShot(clip);
