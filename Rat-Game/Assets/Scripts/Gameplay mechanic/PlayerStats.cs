5afc71ca30aaeee3230502a57a0f0b38cb864e5c
Jacob Kaufman-Warner <145163062+chibbychaps@users.noreply.github.com>
2025-04-08 13:56:30 -0700

    Re-added blood and damage flash
    I added blood splash VFX and red damage flash back into the game.

--- Rat-Game/Assets/Scripts/Gameplay mechanic/PlayerStats.cs
+++ Rat-Game/Assets/Scripts/Gameplay mechanic/PlayerStats.cs
@@ -50,7 +50,7 @@ public class PlayerStats : MonoBehaviour
         {
             Debug.Log("PLAYER DIED!");
             clip = deathSound;
-            SceneManager.LoadScene(5);  // Scene 5 for death
+            SceneManager.LoadScene(3);  // Scene 3 for death
         }
 
         audioSource.PlayOneShot(clip);
