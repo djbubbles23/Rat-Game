a39a52f6e685c89bfc924acf1bc455c9780154ec
Adam Smith <arssmith2023@gmail.com>
2025-04-21 15:38:11 -0700

    delay change

--- Rat-Game/Assets/Scripts/Gameplay mechanic/Playermovemnet.cs
+++ Rat-Game/Assets/Scripts/Gameplay mechanic/Playermovemnet.cs
@@ -279,17 +279,17 @@ public class PlayerMovement : MonoBehaviour
             //animation controller
             playerAnim.runtimeAnimatorController = daggerAnim;
             //speed
-            attackDelay = .75f;
+            attackDelay = .5f;
         }
         if(weaponType == "sword"){
             attackHitbox.transform.localScale = new Vector3(0.22f, 1f, 0.15f);
             playerAnim.runtimeAnimatorController = swordAnim;
-            attackDelay = 1.5f;
+            attackDelay = 1f;
         }
         if(weaponType == "longSword"){
             attackHitbox.transform.localScale = new Vector3(1.64f, 1f, 1.64f);
             playerAnim.runtimeAnimatorController = longSwordAnim;
-            attackDelay = 2.25f;
+            attackDelay = 2f;
         }
     }
 }
