5a107cdb554f4f8cdfe309d145b4b9dfdc999084
Adam Smith <arssmith2023@gmail.com>
2025-05-01 14:39:08 -0700

    .

--- Rat-Game/Assets/Scripts/knockbackScript.cs
+++ Rat-Game/Assets/Scripts/knockbackScript.cs
@@ -1,25 +1,27 @@
-//using System.Numerics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class knockbackScript : MonoBehaviour
 {
     Rigidbody rb;
-    public float knockbackForce; 
+    public float knockbackForce = 0.25f; 
+    public float drag = 1f; // Drag value to slow down the object
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.drag = drag; // Set the drag value on the Rigidbody
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(!collision.gameObject.CompareTag("Ground")){
+        if (!collision.gameObject.CompareTag("Ground"))
+        {
             AddKnockback(collision.transform.position - transform.position, knockbackForce);
         }
     }
+
     public void AddKnockback(Vector3 direction, float force)
     {
-        rb.AddForce(direction * force, ForceMode.Impulse);
+        rb.AddForce(direction.normalized * force, ForceMode.Impulse);
     }
 }
---
+++ Rat-Game/Assets/triggerLock.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class triggerLock : MonoBehaviour
+{   
+    public GameObject cam;
+    public bool isLocked = false;
+    public GameObject[] enemies;
+    public bool isEnmeiesDead = false;
+
+    // On Trigger Enter, lock the camera and set isLocked to true
+    // Spawn the list of enemies and set isEnemiesDead to false
+    // Check in update if those enemies are dead (not found in the scene anymore)
+    // If they are dead, unlock the camera and set isLocked to false
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("Trigger Entered: " + other.name);
+        if(other.tag == "Player"){  
+            cam.GetComponent<CameraController>().enabled = false;
+            isLocked = true;
+            // spawn enemies in enemies array
+            isEnmeiesDead = false;
+        }
+    }
+
+    private void Update(){
+        if(isLocked && isEnmeiesDead){
+            cam.GetComponent<CameraController>().enabled = true;
+            isLocked = false;
+        }
+    }
+
+}
