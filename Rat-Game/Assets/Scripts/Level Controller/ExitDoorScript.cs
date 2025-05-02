07fa5dbff78469c1f05630d65c00c1f833842566
Adam Smith <arssmith2023@gmail.com>
2025-05-01 18:43:10 -0700

    crossfade

--- Rat-Game/Assets/Scripts/Level Controller/ExitDoorScript.cs
+++ Rat-Game/Assets/Scripts/Level Controller/ExitDoorScript.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ExitDoorScript : MonoBehaviour
 {
     [SerializeField] private ZoneControllerScript zoneController;
+    public GameObject crossFadePrefab;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && Input.GetAxis("Jump") == 1)
         {
+            crossFadePrefab.SetActive(false);
+            crossFadePrefab.SetActive(true);
             string nextZone = StateControllerScript.GetNextZone();
             if (!string.IsNullOrEmpty(nextZone))
             {
