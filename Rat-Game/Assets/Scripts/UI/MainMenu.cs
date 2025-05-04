879a5bc743e8dacfc7feefe13a105e4abc3ce0e6
VTNguyen02 <vnguye55@asu.edu>
2025-05-03 20:38:10 -0700

    Opening cutscene

--- Rat-Game/Assets/Scripts/UI/MainMenu.cs
+++ Rat-Game/Assets/Scripts/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("SurfacePipesComp");
     }
 
     public void QuitGame()
