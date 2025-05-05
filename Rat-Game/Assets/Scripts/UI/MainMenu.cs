53d58534cf76a2a63ce1209ea0bc468c93460848
Adam Smith <arssmith2023@gmail.com>
2025-05-04 21:32:21 -0700

    revert

--- Rat-Game/Assets/Scripts/UI/MainMenu.cs
+++ Rat-Game/Assets/Scripts/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Starting cutscene");
+        SceneManager.LoadScene("SurfacePipesComp");
     }
 
     public void QuitGame()
